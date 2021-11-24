e326849a43c5023f03f8b1e1720a4d424896249f
skshaon <shamaunkabir111@gmail.com>
2021-11-24 21:20:25 +0600

    what's app clone full project

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,71 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatar;
+  ChatModel({this.name, this.message, this.time, this.avatar});
+}
+
+List<ChatModel> chatData = [
+  ChatModel(
+    name: "Rahul",
+    message: "i am a Developer",
+    time: "10:20",
+    avatar: "images/rahul.jpg",
+  ),
+  ChatModel(
+    name: "Shamaun Kabir",
+    message: "Hello sir",
+    time: "22:30",
+    avatar: "images/sonam.jpg",
+  ),
+  ChatModel(
+    name: "sayed",
+    message: "kemon asen?",
+    time: "14:23",
+    avatar: "images/sumit.jpg",
+  ),
+  ChatModel(
+    name: "Rohan Jha",
+    message: "Hello",
+    time: "11:45",
+    avatar: "images/rohan.jpg",
+  ),
+  ChatModel(
+    name: "Shakil",
+    message: "i ama studen of computer science ",
+    time: "23:20",
+    avatar: "images/shukla.jpg",
+  ),
+  ChatModel(
+    name: "Raj",
+    message: "i love coding",
+    time: "2:20",
+  ),
+  ChatModel(
+    name: "Sachin",
+    message: "i love the weather of the Canada",
+    time: "21:20",
+    avatar: "images/sachin.jpg",
+  ),
+  ChatModel(
+    name: "Jay",
+    message: "ottawa is the capital of the canda",
+    time: "21:20",
+  ),
+  ChatModel(
+    name: "ittad",
+    message: "Same here",
+    time: "21:20",
+  ),
+  ChatModel(
+    name: "namiul",
+    message: "what's your profession",
+    time: "21:20",
+  ),
+  ChatModel(
+    name: "Kabir",
+    message: "ki obsta vai kemon asen ?",
+    time: "21:20",
+  ),
+];
